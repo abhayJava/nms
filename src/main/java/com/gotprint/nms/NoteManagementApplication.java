703e325f849bb0e4a2cd80a5e89be6b54e3ea88d
Abhay <Abhay.Kumar@mycom-osi.com>
2018-06-29 12:57:27 +0530

    Project Structure

---
+++ src/main/java/com/gotprint/nms/NoteManagementApplication.java
@@ -0,0 +1,15 @@
+package com.gotprint.nms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManagementApplication
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(NoteManagementApplication.class, args);
+   }
+
+}
